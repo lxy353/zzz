58c3ab6ba951708fc4e61517e6436211b17918b4
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2020-04-17 11:17:53 -0400

    Add files via upload

--- zha/Q1.java
+++ zha/Q1.java
@@ -1,27 +1,21 @@
-
 import java.util.Scanner;
 
-/**
- * 验证键盘输入的用户名不能为空，长度大于6，不能有数字。
- * @author xxyh
- *
- */
 public class Q1 {
-	public static void main(String[] args) {
-		System.out.println("请输入用户名(不能为空，长度大于6，不能有数字)：");
-		Scanner scanner =new Scanner(System.in);
-		String str = scanner.nextLine();
-		//判断是否含有数字 (正则表达式)
-		String regex = ".*[0-9].*";  
-		boolean result = str.matches(regex);
-		if(str.equals("")){
-			System.out.println("用户名不能为空！");
-		}else if(str.length()<7){
-			System.out.println("用户名长度必须大于6");
-		}else if(result){
-			System.out.println("用户名不能有数字");
-		}else{
-			System.out.println("用户名为："+str);
-		}
-	}
+	public static void  main (String[] args) throws IllegalScoreException {
+		System.out.println("Give me a score");
+		Scanner scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+	    if (score< 0|| score >=100) {
+	            throw new IllegalScoreException("No range");
+	        }  
+	    }
+}
+class IllegalScoreException extends Exception {
+	  
+	public IllegalScoreException() {
+    }
+   
+    public IllegalScoreException(String message) {
+        super(message);
+    }
 }
--- zha/Q2.java
+++ zha/Q2.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Q2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("give me a int a");
+	Scanner scanner1 = new Scanner(System.in);
+	int a = scanner1.nextInt();
+	System.out.println("give me a int b");
+	Scanner scanner2 = new Scanner(System.in);
+	int b = scanner2.nextInt();
+	System.out.println("give me a int b");
+	Scanner scanner3 = new Scanner(System.in);
+	int c = scanner3.nextInt();
+	isTriangle(a,b,c);
+	}
+	static void isTriangle(int a,int b,int c) {
+		if((a^2)!=(b^2)+(c^2)||(b^2)!=(a^2)+(c^2)||(c^2)!=(a^2)+(b^2));
+		throw new IllegalArgumentException("They cant make triangle");
 	}
 
 }
