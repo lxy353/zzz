720e63cde4250a03e67a367811a7c89c338bae1f
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2020-05-30 14:01:42 -0400

    Add files via upload

---
+++ Thread/Web12306.java
@@ -0,0 +1,36 @@
+package com.lxy;
+/**
+ * 共享资源
+ * @author lxz35
+ *
+ */
+public class Web12306 implements Runnable {
+	//票数
+	private  int ticket=99;
+	public void run() {
+		// TODO Auto-generated method stub
+		while (true) {
+			if (ticket<0) {
+				break;
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+"::"+ticket--);
+		}
+	}
+	public static void main(String[] args) {
+		//一份资源
+		Web12306 web12306= new Web12306();
+		System.out.println(Thread.currentThread().getName());
+		//多个代理
+		new Thread(web12306,"lxy").start();
+		new Thread(web12306,"llx").start();
+		new Thread(web12306,"lll").start();
+	}
+
+
+}
