01ef90eb366d008e600bd0668d6bc564fbc9df8c
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2020-06-23 10:59:58 -0400

    Add files via upload

---
+++ IP/IP01/PortTest.java
@@ -0,0 +1,21 @@
+import java.net.InetSocketAddress;
+/**
+ * 端口
+ * 1.区分软件
+ * 2. 2个字节 0-65525 UDP TCP
+ * 3.同一个协议端口不可以冲突
+ * 4.定义端口越大越好
+ * @author lxz35
+ *
+ */
+
+public class PortTest {
+	public static void main(String[] args) {
+		InetSocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 8080);
+		InetSocketAddress socketAddress2 = new InetSocketAddress("localhost",9000);
+		System.out.println(socketAddress.getHostName());
+		System.out.println(socketAddress2.getHostName());
+		System.out.println(socketAddress.getPort());
+	}
+
+}
