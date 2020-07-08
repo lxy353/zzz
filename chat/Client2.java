fd0f43012d8d1697a8c20276e531e63dad4f8e19
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2020-07-07 20:47:43 -0400

    Add files via upload

---
+++ chat/Client2.java
@@ -0,0 +1,32 @@
+package com.lxy.chat01;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client2 {
+	public static void main(String[] args) throws UnknownHostException, IOException  {
+		System.out.println("-----client-----");
+		Socket clientSocket = new Socket("localhost",9999);
+		// 客户端发送消息
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());		
+		DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+		Boolean isBoolean=true;
+		while (isBoolean) {
+		String msg =bufferedReader.readLine();
+		dos.writeUTF(msg);
+		dos.flush();
+		//获取消息
+		 msg = dis.readUTF();
+		 System.out.println(msg);
+	}
+		 dos.close();
+		 dis.close();
+		 clientSocket.close();
+	}
+}
