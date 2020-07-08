63bc60aaf0c3b9ebb3d1f63634b96e6ba0b1d130
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2020-07-07 20:27:31 -0400

    Add files via upload

---
+++ chat/Chat.java
@@ -0,0 +1,37 @@
+package com.lxy.chat01;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 在线聊天室：客户端
+ * 目标：实现一个客户可以正常首发消息
+ * @author jakel
+ *
+ */
+
+public class Chat {
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----server-----");
+		//1.指定端口 使用server socket 创建服务器
+		ServerSocket socket = new ServerSocket(9999);
+		//2阻塞是等待连接accept
+		Socket clientSocket = socket.accept();
+		System.out.println("一个客户建立了连接");
+		//3接受消息
+		DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+		String msg = dis.readUTF();
+		//4换回消息
+		DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());		
+		dos.writeUTF(msg);
+		//释放资源
+		dos.flush();
+		dos.close();
+		dis.close();
+		clientSocket.close();
+	}
+
+}
