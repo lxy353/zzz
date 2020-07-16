5dad06c4fa1e2956953693156b1c0055ec09124d
XINGYU LIU <47311354+lxy353@users.noreply.github.com>
2020-07-15 20:44:55 -0400

    Add files via upload

--- Server-study/1/ReflectTest.java
+++ Server-study/1/ReflectTest.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * 反射：把java 结构
  * @author java 类中的各种结构（方法，属性，构造器，类型）映射成一个个的java对象
@@ -9,7 +11,7 @@
  *
  */
 public class ReflectTest {
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		//三种方式
 		//1.对象
 		iphone iphoe = new iphone();
@@ -17,16 +19,13 @@ public class ReflectTest {
 		//2.类。class（）
 		clzClass = iphone.class;
 		//3 class for name ("包名。类名")
-		clzClass = clzClass.forName("包名.类名");
+		clzClass = clzClass.forName("Server-study.iphone");
 		//创建对象
-		iphone iphone2= (iphone)clzClass.newInstance();
+		/*iphone iphone2= (iphone)clzClass.newInstance();
+		System.out.println(iphone2);*/
+		iphone iphone2= (iphone)clzClass.getConstructor().newInstance();
 		System.out.println(iphone2);
-		
-		
-		
 	}
-	
-
 }
 class iphone{
 	public iphone() {
---
+++ Server-study/1/XmlTest01.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.ext.DefaultHandler2;
+
+
+/**
+ * 熟悉SAX解析流程
+ * @author jakel
+ *
+ */
+public class XmlTest01 {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		//SAX解析
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		//1.获取解析工厂
+		SAXParser parser = factory.newSAXParser();
+		//2。从解析工程获取解析器
+		//3.加载文档Document 注册处理器
+		//4.编写处理器
+		PersonHandler handler = new PersonHandler();
+		//5 解析
+		parser.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream("p.xml"),handler);
+		
+		
+	}
+
+}
+class PersonHandler extends DefaultHandler2{
+	@Override
+	public void startDocument() throws SAXException {
+		System.out.println("解析文档开始");
+		super.startDocument();
+	}
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		System.out.println(qName+"---解析开始");
+		super.startElement(uri, localName, qName, attributes);
+	}
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		System.out.println(qName+"---解析结束");
+		super.endElement(uri, localName, qName);
+	}
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		String string = new String(ch,start,length).trim();
+		if (string.length()>0) {
+			System.out.println("内容为："+string);	
+		}else {
+			System.out.println("内容为: null");
+		}
+		
+		super.characters(ch, start, length);
+	}
+	@Override
+	public void endDocument() throws SAXException {
+		System.out.println("解析文档结束");
+		super.endDocument();
+	}
+	
+}
